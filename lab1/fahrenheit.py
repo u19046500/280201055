4504a36f3b970360df3640c43303a5e0c94a55b1
u19046500 <eraykbd7@gmail.com>
2020-10-19 09:46:40 +0000

    numbers

---
+++ lab1/fahrenheit.py
@@ -0,0 +1,3 @@
+celcius = int(input("Enter Celcius:"))
+fahrenheit = celcius * 1.8 + 32
+print("Fahrenheit:", fahrenheit)
---
+++ lab1/hypotenus.py
@@ -0,0 +1,5 @@
+
+a = int(input("Enter perpendicular edge 1:"))
+b = int(input("Enter perpendicular edge 2:"))
+hypotenus = (a**2 + b**2)**0.5
+print(hypotenus)
